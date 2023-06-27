d9a768a019f7b677604f4ed62696c037e7dfff00
Aliev Nurlan <nualiev420@gmail.com>
2023-06-27 22:25:07 +0400

    add view statuses

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class StatusModel(models.Model):
+
+    name = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-
+from task_manager.statuses.models import StatusModel
 
 
 class StatusesView(View):
-    def get(self, request, *args, **kwargs):
 
-        return render(request, 'statuses/index.html')
+    def get(self, request, *args, **kwargs):
+        statuses = StatusModel.objects.all()
+        return render(request, 'statuses/index.html', {'statuses': statuses})
 
 
 class CreateStatusesView(View):
