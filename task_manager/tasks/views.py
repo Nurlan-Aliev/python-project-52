2af119e2fec496950352e88ef3b78e26efdc731b
Aliev Nurlan <nualiev420@gmail.com>
2023-06-30 12:13:50 +0400

    refactor code

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -4,8 +4,10 @@ from task_manager.utils import CheckAuthentication
 
 
 class TaskView(CheckAuthentication, View):
+    template_name = 'tasks/tasks.html'
+
     def get(self, request, *args, **kwargs):
-        return render(request, 'tasks/tasks.html')
+        return render(request, self.template_name)
 
 
 class CreateTaskView(CheckAuthentication, View):
